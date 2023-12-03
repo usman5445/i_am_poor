c00d9ddc16e5314b6180e698fab4b3d901c1645d
usman5445 <usman@infiniticube.in>
2023-12-03 12:51:03 +0530

    final push

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        title: Text('I Am Poor'),
+        backgroundColor: Colors.brown[900],
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/broke.png'),
+        ),
+      ),
+    ),
+  ));
 }
